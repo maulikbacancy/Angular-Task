24d71ee706839dac4e5f29b1316c81c00aaf4af4
Maulik Gevariya <maulikgevariya@users.noreply.github.com>
2021-05-13 14:10:42 +0530

    selection property updated

--- diagram/src/app/app.component.ts
+++ diagram/src/app/app.component.ts
@@ -126,8 +126,6 @@ export class AppComponent {
 
     public checkObjectValue() {
       console.log(this.diagramData);
-      console.log(this.diagram);
-            
     }
 
     public propertyChange() {
